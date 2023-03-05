6f3c05d50b86f7c63d15254b173f9006c8997adc
Thiago Augusto <wstagf@gmail.com>
2023-03-05 20:27:31 -0300

    Movido componentes para ficar dentro do modulo correto

--- src/app/presentation/view/shared/shared.module.ts
+++ src/app/presentation/view/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';  
-import { LogoComponent } from './components/logo/logo.component';
-import { ChatMenuItemComponent } from './components/chat-menu-item/chat-menu-item.component';
-import { BankinkMenuItemComponent } from './components/banking-menu-item/banking-menu-item.component';
-import { ConfigMenuItemComponent } from './components/config-item/config-menu-item.component';
-import { MenuItemComponent } from './components/menu-item/menu-item.component';
-import { SearchMenuItemComponent } from './components/search-item/search-menu-item.component';
-import { StarMenuItemComponent } from './components/start-item/star-menu-item.component';
+import { LogoComponent } from './menu/components/logo/logo.component';
+import { ChatMenuItemComponent } from './menu/components/chat-menu-item/chat-menu-item.component';
+import { BankinkMenuItemComponent } from './menu/components/banking-menu-item/banking-menu-item.component';
+import { ConfigMenuItemComponent } from './menu/components/config-item/config-menu-item.component';
+import { MenuItemComponent } from './menu/components/menu-item/menu-item.component';
+import { SearchMenuItemComponent } from './menu/components/search-item/search-menu-item.component';
+import { StarMenuItemComponent } from './menu/components/start-item/star-menu-item.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
